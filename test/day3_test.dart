ebf22efa205f26b5ecb03b05a6ce73d796669b90
Simon Kincaid <simon.kincaid@kintronix.com>
2023-12-03 21:11:51 +0000

    ugh

--- test/day3_test.dart
+++ test/day3_test.dart
@@ -33,6 +33,7 @@ void main() {
     ];
   }
 
+  // This is a rubbish function would refactor if bothered
   List<(int, int)> getBorderLocationsForNumeric(Match match, int row) {
     // Make a list of records for the numerics
     // so if its '123' we will return three
@@ -71,11 +72,9 @@ void main() {
         // The assumption was that we wanted to 'get all neighbours'
         // but actually we want to get three sets of records
         var rowAbove = getBorderLocationsForNumeric(numeric, row - 1);
-        // var rowNow = getBorderLocationsForNumeric(numeric, row - 1);
         var rowNow = [(row, numeric.start - 1), (row, numeric.end)];
         var rowBelow = getBorderLocationsForNumeric(numeric, row + 1);
         var joined = [...rowAbove, ...rowNow, ...rowBelow];
-        bool hasNeighbouringSymbol = false;
         for ((int, int) target in joined) {
           if (isSymbol(target.$1, target.$2)) {
             print(
@@ -88,6 +87,108 @@ void main() {
         }
       }
     });
-    print(puzzleInputProcessed);
+    print(puzzleInputProcessed); // correct at 537832
+  });
+
+  test('D3:P2: getAllNeighbours returns as expected', () {
+    // List<String> puzzleInput = [
+    //   r'467..114..',
+    //   r'...*......',
+    //   r'..35..633.',
+    //   r'......#...',
+    //   r'617*......',
+    //   r'.....+.58.',
+    //   r'..592.....',
+    //   r'......755.',
+    //   r'...$.*....',
+    //   r'.664.598..',
+    // ];
+    List<(int, int)> gears = [];
+
+    puzzleInput.asMap().forEach((int row, String col) {
+      var gearSearch = RegExp(r'(\*)').allMatches(col);
+      for (final Match gear in gearSearch) {
+        // because its a single char its slightly easier
+        gears.add((row, gear.start));
+      }
+      // convert matches into
+    });
+
+    print(gears);
+
+    int total = 0;
+
+    for ((int, int) location in gears) {
+      List<int> numAdjacentNumerics = [];
+
+      (int, int) gear = (location.$1, location.$2);
+
+      var from = gear.$1 - 1;
+      var to = gear.$1 + 1;
+
+      for (var row = from; row <= to; row++) {
+        // Check in the row above
+        var numerics = RegExp(r'(\d+)').allMatches(puzzleInput[(row)]);
+
+        (int, int, int) collisionDetectionArea =
+            (gear.$2 - 1, gear.$2, gear.$2 + 1);
+
+        numerics.forEach((element) {
+          List<int> numericIndices = [];
+
+          for (var i = element.start; i <= element.end - 1; i++) {
+            numericIndices.add(i);
+          }
+
+          bool collision = numericIndices.any((index) =>
+              index == collisionDetectionArea.$1 ||
+              index == collisionDetectionArea.$2 ||
+              index == collisionDetectionArea.$3);
+
+          if (collision) {
+            numAdjacentNumerics.add(int.tryParse(element.group(1) ?? '') ?? 0);
+            print('found collision ${element.group(1)}');
+          }
+        });
+      }
+      if (numAdjacentNumerics.length == 2) {
+        var multiple = numAdjacentNumerics[0] * numAdjacentNumerics[1];
+        print(
+            'Found a lovely gear with two adjacents at ${gear.$1},${gear.$2} that multiply to form $multiple');
+        total += multiple;
+      }
+
+      // current
+
+      // below
+    }
+
+    print(total);
+
+    // var content = puzzleInput[row][col];
+
+    // locations.forEach((element) {});
+
+    // var rowAbove = getBorderLocationsForNumeric(numeric, row - 1);
+    // var rowNow = [(row, numeric.start - 1), (row, numeric.end)];
+    // var rowBelow = getBorderLocationsForNumeric(numeric, row + 1);
+    // var joined = [...rowAbove, ...rowNow, ...rowBelow];
+
+    // for these locations find how many numbers are around it
+    // for each gear point you can
+    //
+
+    // Locate *
+
+    // Foreach
+    // Calculate container to get list of location records
+    // You could see which contains a numeric
+    // but at the same time you still need to work out
+    // what group
+    // The only thing you can do is have a long list of all location records for numerics
+    // which i can do
+    //
+
+    // You could get
   });
 }
